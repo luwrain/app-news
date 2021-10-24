eab7edf4a54d25f72caa49118a28e76f4814d6a5
Michael Pozhidaev <msp@luwrain.org>
2021-10-24 14:54:39 +0700

    news2: Minor fixes

--- src/main/java/org/luwrain/app/news2/PropertiesLayout.java
+++ src/main/java/org/luwrain/app/news2/PropertiesLayout.java
@@ -4,28 +4,26 @@ package org.luwrain.app.news2;
 import java.util.*;
 
 import org.luwrain.core.*;
-import org.luwrain.core.events.*;
 import org.luwrain.controls.*;
-import org.luwrain.controls.reader.*;
 import org.luwrain.pim.news.*;
-import org.luwrain.pim.*;
 import org.luwrain.app.base.*;
 
 final class PropertiesLayout extends LayoutBase
 {
-static private final String
-    ORDER_INDEX = "order-index";
+    static private final String
+	NAME = "name",
+	ORDER_INDEX = "order-index";
 
     private final App app;
-			  final FormArea formArea;
+    final FormArea formArea;
 
-			  PropertiesLayout(App app, NewsGroup group)
-			  {
-			      super(app);
-			      NullCheck.notNull(group, "group");
-			      this.app = app;
-			      this.formArea = new FormArea(getControlContext()) ;
-			      			      	final List<String> urls = group.getUrls();
+    PropertiesLayout(App app, NewsGroup group)
+    {
+	super(app);
+	NullCheck.notNull(group, "group");
+	this.app = app;
+	this.formArea = new FormArea(getControlContext()) ;
+	final List<String> urls = group.getUrls();
 	final String[] urlLines;
 	if (urls != null && !urls.isEmpty())
 	{
@@ -36,8 +34,8 @@ static private final String
 	    urlLines = res.toArray(new String[res.size()]);
 	} else
 	    urlLines = new String[0];
-	formArea.addEdit("name", app.getStrings().groupPropertiesName(), group.getName());
-	formArea.addEdit("order-index", app.getStrings().groupPropertiesOrderIndex(), "" + group.getOrderIndex());
+	formArea.addEdit(NAME, app.getStrings().groupPropertiesName(), group.getName());
+	formArea.addEdit(ORDER_INDEX, app.getStrings().groupPropertiesOrderIndex(), "" + group.getOrderIndex());
 	formArea.activateMultilineEdit(app.getStrings().groupPropertiesUrls(), urlLines, true);
 	setAreaLayout(formArea, actions());
     }
@@ -45,40 +43,39 @@ static private final String
     private boolean save(NewsGroup group)
     {
 	NullCheck.notNull(group, "group");
-
-			      			    final String name = formArea.getEnteredText("name");
-			    if (name.trim().isEmpty())
-			    {
-				app.message(app.getStrings().groupPropertiesNameMayNotBeEmpty(), Luwrain.MessageType.ERROR);
-				return false;
-			    }
-			    group.setName(name);
-			    if (formArea.getEnteredText("order-index").trim().isEmpty())
-			    {
-				app.message(app.getStrings().groupPropertiesInvalidOrderIndex(), Luwrain.MessageType.ERROR);
-				return false;
-			    }
-			    final int orderIndex;
-			    try {
-				orderIndex = Integer.parseInt(formArea.getEnteredText(ORDER_INDEX));
-			    }
-			    catch(NumberFormatException e)
-			    {
-				app.message(app.getStrings().groupPropertiesInvalidOrderIndex(), Luwrain.MessageType.ERROR);
-				return false;
-			    }
-			    if (orderIndex < 0)
-			    {
-				app.message(app.getStrings().groupPropertiesInvalidOrderIndex(), Luwrain.MessageType.ERROR);
-				return false;
-			    }
-			    group.setOrderIndex(orderIndex);
-			    final List<String> urls = new ArrayList<>();
-			    for(String s: formArea.getMultilineEditLines())
-				if (!s.trim().isEmpty())
-				    urls.add(s.trim());
-			    group.setUrls(urls);
-			    group.save();
+	final String name = formArea.getEnteredText(NAME);
+	if (name.trim().isEmpty())
+	{
+	    app.message(app.getStrings().groupPropertiesNameMayNotBeEmpty(), Luwrain.MessageType.ERROR);
+	    return false;
+	}
+	group.setName(name);
+	if (formArea.getEnteredText("order-index").trim().isEmpty())
+	{
+	    app.message(app.getStrings().groupPropertiesInvalidOrderIndex(), Luwrain.MessageType.ERROR);
+	    return false;
+	}
+	final int orderIndex;
+	try {
+	    orderIndex = Integer.parseInt(formArea.getEnteredText(ORDER_INDEX));
+	}
+	catch(NumberFormatException e)
+	{
+	    app.message(app.getStrings().groupPropertiesInvalidOrderIndex(), Luwrain.MessageType.ERROR);
+	    return false;
+	}
+	if (orderIndex < 0)
+	{
+	    app.message(app.getStrings().groupPropertiesInvalidOrderIndex(), Luwrain.MessageType.ERROR);
+	    return false;
+	}
+	group.setOrderIndex(orderIndex);
+	final List<String> urls = new ArrayList<>();
+	for(String s: formArea.getMultilineEditLines())
+	    if (!s.trim().isEmpty())
+		urls.add(s.trim());
+	group.setUrls(urls);
+	group.save();
 	return true;
     }
 }
