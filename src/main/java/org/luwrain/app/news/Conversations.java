75e43c09ed24e3828d2a218b0593cc4689fbcf28
Michael Pozhidaev <msp@luwrain.org>
2021-10-25 04:37:19 +0700

    Removing news2

--- src/main/java/org/luwrain/app/news/Conversations.java
+++ src/main/java/org/luwrain/app/news/Conversations.java
@@ -1,37 +1,23 @@
-/*
-   Copyright 2012-2019 Michael Pozhidaev <msp@luwrain.org>
 
-   This file is part of LUWRAIN.
-
-   LUWRAIN is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public
-   License as published by the Free Software Foundation; either
-   version 3 of the License, or (at your option) any later version.
-
-   LUWRAIN is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
-*/
-
-package org.luwrain.app.news;
+package org.luwrain.app.news2;
 
 import java.net.*;
 
 import org.luwrain.core.*;
 import org.luwrain.popups.*;
 
+import org.luwrain.app.news.Strings;
+
 final class Conversations
 {
     private final Luwrain luwrain;
     private final Strings strings;
 
-    Conversations(Luwrain luwrain, Strings strings)
+    Conversations(App app)
     {
-	NullCheck.notNull(luwrain, "luwrain");
-	NullCheck.notNull(strings, "strings");
-	this.luwrain = luwrain;
-	this.strings = strings;
+	NullCheck.notNull(app, "app");
+	this.luwrain = app.getLuwrain();
+	this.strings = app.getStrings();
     }
 
     boolean confirmGroupDeleting(GroupWrapper wrapper)
